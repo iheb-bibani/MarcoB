bfdb5e1573c5c1c4099d3e826ae52a0b61f237bb
iheb <37037732+iheb-bibani@users.noreply.github.com>
2023-03-11 14:47:29 +0100

    commit

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if assets == 'FX' :
     st.plotly_chart(fig)
 
     # Graph 2
-    st.subheader('Graph 2 : dd VS cumpnl')
+    st.subheader('Graph 2 : cumpnl vs dd')
     
     # create first trace
     trace1 = go.Scatter(x=df['date'], y=df['dd'], name='dd',line=dict(color='red'))
